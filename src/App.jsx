a1d03a8be580dc7eaeda758580d9eabbdfa69b3c
Valentina <inmortal1201@gmail.com>
2017-10-23 20:39:53 +0300

    настройки webpack.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './scss/main.scss';
+
 import axios from 'axios';
 
 import Todo from './components/TodoItem';
--- webpack.config.js
+++ webpack.config.js
@@ -29,14 +29,16 @@ module.exports = {
             ]
       },
       {
-        test: /\.css?$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        test: /\.scss$/,
+        use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
       }
     ]
   },
-
   devtool: 'eval-source-map',
-
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   }
